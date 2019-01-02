bf2e700aaa7433375be7b98910dfb452789ddcd1
William Cao <wcao00@stuy.edu>
2019-01-02 12:16:46 -0500

    createed CSVRepresentation class

---
+++ CSVRepresentation.java
@@ -0,0 +1,9 @@
+import java.nio.file.Path;
+
+public class CSVRepresentation {
+    private final Path pathToCSV;
+
+    public CSVRepresentation(Path pathToCSV){
+        this.pathToCSV = pathToCSV;
+    }
+}
--- Head.java
+++ Head.java
@@ -1,6 +1,12 @@
+import java.nio.file.Path;
+
 /**
  * Head of the program.
  */
 public class Head{
+    private final CSVRepresentation csvRepresentation;
 
+    public Head(Path pathToCSV){
+        this.csvRepresentation = new CSVRepresentation(pathToCSV);
+    }
 }
