68cacf7190f5fbd5e1e1f3ab1cea49510ea912a3
William Cao <wcao00@stuy.edu>
2019-01-03 10:56:34 -0500

    made constructor private and use factory methods

--- CSVRow.java
+++ CSVRow.java
@@ -7,7 +7,7 @@ public class CSVRow {
     /**
      * A node represents a cell in the csv file. This is what is between the comma(s)
      */
-    private class CSVNode{
+    private static class CSVNode{
         /**
          * The data stored in the node.
          */
@@ -22,15 +22,48 @@ public class CSVRow {
         private CSVNode next;
 
         /**
-         * Creates a cell in the row.
+         * Creates a new cell in the row.
          * @param data Data that will be stored in the cell.
          * @param previous Reference to the previous node
          * @param next Reference to the
          */
-        CSVNode(String data, CSVNode previous, CSVNode next){
+        private CSVNode(String data, CSVNode previous, CSVNode next){
             this.data = data;
             this.previous = previous;
             this.next = next;
+        }
+
+        /**
+         * Static Factory Method to get an instance of this class.
+         * Creates a new cell given the data to be stored, previous and next node. 
+         * @param data Data to be stored on the list.
+         * @param previous Previous node; null if this is the first node
+         * @param next Next node; null if this is the last node
+         * @return A new node
+         */
+        public static CSVNode newInstance(String data, CSVNode previous, CSVNode next){
+            return new CSVNode(data, previous, next);
+        }
+
+        /**
+         * Static Factory Method to get an instance of this class
+         * Creates a new cell given the string representation of the node.
+         * @param stringRepresentation String representation of the node defined by {@link #toString()}
+         * @param previous Previous node, null if this is the first node
+         * @param next Next node, null if this is the last node
+         * @return A new node from the given string representation
+         */
+        public static CSVNode of(String stringRepresentation, CSVNode previous, CSVNode next){
+            return new CSVNode(stringRepresentationToData(stringRepresentation), previous, next);
+        }
+
+        /**
+         * Converts the string representation of the node defined by {@link #toString()} to data.
+         * @param stringRepresentation String representation of the cell
+         * @return Data to be stored in the cell.
+         */
+        private static String stringRepresentationToData(String stringRepresentation){
+
         }
 
         /**
