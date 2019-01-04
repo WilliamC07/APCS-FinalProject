9b40d55b2b600bbe3256ea48d00216c0e231550f
William Cao <wcao00@stuy.edu>
2019-01-04 15:29:09 -0500

    can read the file given by the user and make a linkedlist of rows from it

--- CSVRepresentation.java
+++ CSVRepresentation.java
@@ -1,11 +1,29 @@
 import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class CSVRepresentation {
     private final Path pathToCSV;
+    private final CSVAccess csvAccess;
+    private final LinkedList<CSVRow> rows;
 
     public CSVRepresentation(Path pathToCSV){
         this.pathToCSV = pathToCSV;
+        this.csvAccess = new CSVAccess(pathToCSV);
+        this.rows = readCSV();
     }
 
+    /**
+     * Reads the CSV file given by the user and creates a LinkedList from it.
+     * @return A LinkedList that contains a row of cells of the CSV file.
+     */
+    private LinkedList<CSVRow> readCSV(){
+        LinkedList<CSVRow> rows = new LinkedList<>();
+        Scanner fileReader = csvAccess.readCSV();
+        while(fileReader.hasNextLine()){
+            rows.add(CSVRow.createNewRowFromString(fileReader.nextLine()));
+        }
+        return rows;
+    }
 
 }
