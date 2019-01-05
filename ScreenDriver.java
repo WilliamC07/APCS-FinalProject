093848273ef38b7ff360bfc4bb29030b77da7a1d
Samson Badlia <sbadlia00@stuy.edu>
2019-01-05 12:10:26 -0500

    Added test for getTable

--- ScreenDriver.java
+++ ScreenDriver.java
@@ -1,14 +1,16 @@
 public class ScreenDriver{
 
   public static void main(String[] args) {
-    System.out.println("Should return ap");
+    /*System.out.println("Should return ap");
     System.out.println("Returned:" + Screen.fitSpace("apple", 2));
     System.out.println("Should return app");
     System.out.println("Returned:" + Screen.fitSpace("apple", 3));
     System.out.println("Should return ap p");
     System.out.println("Returned:" + Screen.fitSpace("ap ple", 4));
     System.out.println("Should return horse");
-    System.out.println("Returned:" + Screen.fitSpace("horse", 5));
+    System.out.println("Returned:" + Screen.fitSpace("horse", 5));*/
+
+    System.out.println("Returned:" +  "\n" + Screen.getTable(40,10,10));
 
     }
 
