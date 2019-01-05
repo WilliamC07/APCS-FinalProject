1b8b8e7146e53e3d3d27d50bf23e1c0f9ed7ca3e
Samson Badlia <sbadlia00@stuy.edu>
2019-01-05 12:09:58 -0500

    Wrote getTable (not working properly right now)

--- Screen.java
+++ Screen.java
@@ -19,4 +19,43 @@ public class Screen{
       return s;
     }
 
+    /*This is how it should look like on a 40 by 10 grid
+                |          |          |
+      ----------------------------------------
+                |          |          |
+      ----------------------------------------
+                |          |          |
+      ----------------------------------------
+                |   	     |          |
+      ----------------------------------------
+                |          |          |
+      ----------------------------------------
+
+The cells are 10 spaces (so it can fit that many characters)*/
+
+  public static String getTable(int columns, int rows, int cellspacing){
+    String s = "";
+    for (int j = 0; j < rows / 2; j++){
+      for (int x = 0; x < (columns / cellspacing); x++){
+        int i = 0;
+        while(i < cellspacing){
+          s += "_";
+          i++;
+        }
+        if (x < (columns / cellspacing) - 1){
+          s += "|";
+        }
+      }
+      s += "\n";
+    }
+    return s;
+  }
+/*
+Make sure that after each line, there is a '\n'
+
+Make sure the last line is always a line of dashes.
+
+For not just just spaces to separate the '|' We will later modify it to show content
+
+*/
 }
