52b3d43afa6f61bcfafdc028d1081a522a29ff9c
William Cao <wcao00@stuy.edu>
2019-01-05 12:20:18 -0500

    made skeleton class to convert Key input to a string

---
+++ CommandBuilder.java
@@ -0,0 +1,13 @@
+import com.googlecode.lanterna.input.Key;
+
+public class CommandBuilder {
+    private StringBuilder builder = new StringBuilder();
+    public void addChar(Key key){
+
+    }
+
+    @Override
+    public String toString(){
+        return null;
+    }
+}
