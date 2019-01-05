f402280597190ee116314bcc7cb85c2362b38d21
William Cao <wcao00@stuy.edu>
2019-01-05 11:40:32 -0500

    set up skeleton of the class

--- Screen.java
+++ Screen.java
@@ -1,3 +1,31 @@
-public class Screen extends Thread{
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.Terminal;
 
+import java.util.ArrayDeque;
+
+public class Screen extends Thread{
+    private final CSVRepresentation csvRepresentation;
+
+    /**
+     * Constructor
+     * @param csvRepresentation The CSV to be shown to the screen
+     */
+    public Screen(CSVRepresentation csvRepresentation){
+        this.csvRepresentation = csvRepresentation;
+    }
+
+    /**
+     * This updates the screen and listens to the user keystrokes.
+     */
+    @Override
+    public void run(){
+        Terminal terminal = TerminalFacade.createTextTerminal();
+        boolean isRunning = true;
+
+        terminal.enterPrivateMode();
+
+        while(isRunning){
+
+        }
+    }
 }
