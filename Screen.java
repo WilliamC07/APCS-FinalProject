dea2b9d4f6937d405ccbe9e3e0918672e43149fb
William Cao <wcao00@stuy.edu>
2019-01-05 12:17:32 -0500

    made skeleton for run()

--- Screen.java
+++ Screen.java
@@ -1,4 +1,5 @@
 import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.util.ArrayDeque;
@@ -21,11 +22,42 @@ public class Screen extends Thread{
     public void run(){
         Terminal terminal = TerminalFacade.createTextTerminal();
         boolean isRunning = true;
-
         terminal.enterPrivateMode();
+        // Prevents the user from writing text to the screen directly, text only goes in the last line
+        terminal.setCursorVisible(false);
 
         while(isRunning){
+            // Removes previous content
+            terminal.clearScreen();
 
+            // Show the table onto the screen
+            updateScreen(terminal);
+
+            // Process the keystrokes if there are any
+            Key key = terminal.readInput();
+            if(key != null){
+
+            }
+
+            // Sleep so the program doesn't take up the entire cpu
+            try{
+                Thread.sleep(8);
+            }catch(InterruptedException e){
+                // Do nothing
+            }
+        }
+    }
+
+    /**
+     * Shows the new content onto the screen. This assumes that the screen has been cleared already and is in private
+     * mode.
+     * @param terminal Terminal in which to output the characters
+     */
+    private void updateScreen(Terminal terminal){
+        // Add the new content (or old content if nothing changed)
+        String table = "Sloths are cool"; // TODO update to use samson's method
+        for(int i = 0; i < table.length(); i++){
+            terminal.putCharacter(table.charAt(i));
         }
     }
 }
