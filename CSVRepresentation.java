761211d85b673620c3ba0f876c81a7465ead70c8
William Cao <wcao00@stuy.edu>
2019-01-05 22:22:37 -0500

    fixed bug of not making cells properly

--- CSVRepresentation.java
+++ CSVRepresentation.java
@@ -13,6 +13,15 @@ public class CSVRepresentation {
         this.rows = readCSV();
     }
 
+    public synchronized String getValue(int column, int row){
+        try{
+            return rows.get(row).get(column).toString();
+        }catch(IndexOutOfBoundsException e){
+            // This means the cell doesn't exists, so we can just return an empty string
+            return "";
+        }
+    }
+
     /**
      * Reads the CSV file given by the user and creates a LinkedList from it.
      * @return A LinkedList that contains a row of cells of the CSV file.
