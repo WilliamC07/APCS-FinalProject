c268f204f6fe68856b7febfa4d976b1cabdf74fc
William Cao <wcao00@stuy.edu>
2019-01-07 10:57:16 -0500

    made skeleton class

---
+++ HandleCommand.java
@@ -0,0 +1,10 @@
+public class HandleCommand {
+    private final CSVRepresentation csvRepresentation;
+    public HandleCommand(CSVRepresentation csvRepresentation){
+        this.csvRepresentation = csvRepresentation;
+    }
+
+    public void set(int column, int row, String value){
+
+    }
+}
