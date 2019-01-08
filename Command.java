402ea05d09df79a54036fe51c05f5bcd8e1997df
William Cao <wcao00@stuy.edu>
2019-01-08 10:52:21 -0500

    javadoc

--- Command.java
+++ Command.java
@@ -1,9 +1,20 @@
+/**
+ * This is the command given by the user. Everything the user types in should be made into this class. This
+ * will be added to the stack and all information done on this will.
+ */
 public class Command {
     private String oldValue;
     private String newValue;
     private int row;
     private int column;
 
+    /**
+     * Constructs an command
+     * @param oldValue Previous value
+     * @param newValue New value
+     * @param row Row of edit done to
+     * @param column Column of edit done to
+     */
     public Command(String oldValue, String newValue, int row, int column) {
         this.oldValue = oldValue;
         this.newValue = newValue;
