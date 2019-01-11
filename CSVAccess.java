22084290da2b30fec5403a7a559ebc5a0e4660e7
William Cao <wcao00@stuy.edu>
2019-01-10 21:17:55 -0500

    does not save trailing lines that only contains comma(s)

--- CSVAccess.java
+++ CSVAccess.java
@@ -1,6 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.Scanner;
@@ -30,16 +30,56 @@ public class CSVAccess {
      */
     public void saveCSV(LinkedList<CSVRow> rows){
         try(FileWriter fileWriter = new FileWriter(pathToCSV.toFile());
-            PrintWriter printWriter = new PrintWriter(fileWriter)){
+            BufferedWriter writer = new BufferedWriter(fileWriter)){
             int largestRowSize = simplifyRows(rows);
-            for(CSVRow row : rows){
-                printWriter.println(row.getFileRepresentation(largestRowSize));
+            // This contains everything that needs to be saved to disk.
+            // We are using it for Last In First Out (LIFO) since we are reading the rows backwards
+            LinkedList<String> parts = new LinkedList<>();
+
+            // Loop through backwards so we can remove useless lines, those are lines that only contain comma(s) and
+            // are not between lines that contain characters other than comma(s).
+            // If this is true, that means all lines need to be kept
+            boolean keepMove = false;
+            for(int i = rows.size() - 1; i >= 0; i--){
+                String representation = rows.get(i).getFileRepresentation(largestRowSize);
+                // Only add an additional line if there is something to add (not only commas)
+                // The row is most likely contains the cell that was deleted
+                // We do have to keep an empty line if it is between two lines that are not empty
+                if(keepMove || !isEmptyRow(representation)){
+                    // Each row ends with a end of line delimiter
+                    parts.addFirst(representation);
+                    // All other lines need to be kept (so there isn't a gap)
+                    keepMove = true;
+                }
+            }
+
+            // Go through the stack to write to disk
+            String part;
+            while((part = parts.pollFirst()) != null){
+                writer.write(part);
+                writer.newLine();
             }
         }catch(IOException e){
             // Could not save the file
             System.out.println("Fail to save");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Checks if the row is empty, that is if the string representation of the row (what will be written to disk) only
+     * contains comma(s). Commas are the delimiter for cells.
+     * @param representation The string representation of the cell that will be written to disk. No "\n" allow.
+     * @return True if the row only contains comma(s); false if the row contains other characters
+     */
+    private boolean isEmptyRow(String representation){
+        for(int i = 0; i < representation.length(); i++){
+            if(representation.charAt(i) != ','){
+                return false;
+            }
+        }
+        // If the row only contains commas, it is technically empty
+        return true;
     }
 
     /**
