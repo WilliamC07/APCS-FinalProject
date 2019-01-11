8350ad8a035eedd627fed3b9351308a545eb8c18
William Cao <wcao00@stuy.edu>
2019-01-10 20:22:09 -0500

    saving files will no longer add trailing commas if unncessary

--- CSVAccess.java
+++ CSVAccess.java
@@ -31,13 +31,32 @@ public class CSVAccess {
     public void saveCSV(LinkedList<CSVRow> rows){
         try(FileWriter fileWriter = new FileWriter(pathToCSV.toFile());
             PrintWriter printWriter = new PrintWriter(fileWriter)){
+            int largestRowSize = simplifyRows(rows);
             for(CSVRow row : rows){
-                printWriter.println(row.toString());
+                printWriter.println(row.getFileRepresentation(largestRowSize));
             }
         }catch(IOException e){
             // Could not save the file
             System.out.println("Fail to save");
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Removes all ending the cells that are empty located at the end of each row.
+     * @param rows List of rows
+     * @return The greatest number of columns
+     */
+    private int simplifyRows(LinkedList<CSVRow> rows){
+        int largest = 0;
+        for(CSVRow row : rows){
+            // removes excess cells
+            row.simplify();
+            // Keep track of the row size
+            if(row.size() > largest){
+                largest = row.size();
+            }
+        }
+        return largest;
     }
 }
