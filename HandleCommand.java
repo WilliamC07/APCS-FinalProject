9755df2352064809c3bbb515f61050a6f0a1138d
Samson Badlia <sbadlia00@stuy.edu>
2019-01-13 09:52:57 -0500

    Handled when user wants to subtract and when user provides bad inputs

--- HandleCommand.java
+++ HandleCommand.java
@@ -10,46 +10,55 @@ public class HandleCommand {
     public void handle(String command){
         String[] elements = command.split(" ");
         // Make it upper case for easier comparison
-        switch(elements[0].toUpperCase()){
-          //checks what the user wants to do and handles them accordingly
-            case "SET":
-            {
-                set(Integer.parseInt(elements[1]), Integer.parseInt(elements[2]), elements[3]);
-            }
-                break;
-            case "SWAP":
-            {
-              swap(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]));
-            }
-                break;
-            case "ADD":
-            {
-              add(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]),Integer.parseInt(elements[5]),Integer.parseInt(elements[6]));
-            }
-                break;
-            case "MULTIPLY":
-            {
-              multiply(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]),Integer.parseInt(elements[5]),Integer.parseInt(elements[6]));
-            }
-                break;
-            case "DIVIDE":
-            {
-              division(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]),Integer.parseInt(elements[5]),Integer.parseInt(elements[6]));
-            }
-                break;
-            case "REMOVE":
-            case "RM":
-            {
-                remove(Integer.parseInt(elements[1]), Integer.parseInt(elements[2]));
-            }
-                break;
-            case "UNDO":
-                csvRepresentation.undo();
-                break;
-            default:
-                // Don't know what kind of command, TODO: Tell the user
+        try{
+          switch(elements[0].toUpperCase()){
+            //checks what the user wants to do and handles them accordingly
+              case "SET":
+              {
+                  set(Integer.parseInt(elements[1]), Integer.parseInt(elements[2]), elements[3]);
+              }
+                  break;
+              case "SWAP":
+              {
+                swap(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]));
+              }
+                  break;
+              case "ADD":
+              {
+                add(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]),Integer.parseInt(elements[5]),Integer.parseInt(elements[6]));
+              }
+                  break;
+              case "SUBTRACT":
+              {
+                subtract(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]),Integer.parseInt(elements[5]),Integer.parseInt(elements[6]));
+              }
+                  break;
+              case "MULTIPLY":
+              {
+                multiply(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]),Integer.parseInt(elements[5]),Integer.parseInt(elements[6]));
+              }
+                  break;
+              case "DIVIDE":
+              {
+                division(Integer.parseInt(elements[1]),Integer.parseInt(elements[2]),Integer.parseInt(elements[3]),Integer.parseInt(elements[4]),Integer.parseInt(elements[5]),Integer.parseInt(elements[6]));
+              }
+                  break;
+              case "REMOVE":
+              case "RM":
+              {
+                  remove(Integer.parseInt(elements[1]), Integer.parseInt(elements[2]));
+              }
+                  break;
+              case "UNDO":
+                  csvRepresentation.undo();
+                  break;
+              default:
+                  // Don't know what kind of command, TODO: Tell the user
+          }
         }
-    }
+        catch(NumberFormatException e){
+        }
+      }
 
     /**
      * Sets the value of a cell to the value given by the user
@@ -99,6 +108,28 @@ public class HandleCommand {
       //finds the sum as an int and then converted into a string
       int sum = Integer.parseInt(num1) + Integer.parseInt(num2);
       String s = sum + "";
+      Command c = new Command(oldval, s , storeCol, storeRow);
+      csvRepresentation.pushCommand(c);
+    }
+
+    /**
+     * Subtracts the values inside 2 given cells into the 3rd cell.
+     * @param col1     Column of first cell
+     * @param row1     Row of first cell
+     * @param col2     Column of second cell
+     * @param row2     Row of second cell
+     * @param storeCol Column of cell where data will be stored
+     * @param storeRow Row of cell where data will be stored
+     */
+    private void subtract(int col1, int row1, int col2, int row2, int storeCol, int storeRow){
+      //keeps track of the old value of the cell where the sum will be stored
+      String oldval = csvRepresentation.getValue(storeCol,storeRow);
+      //num1 and num2 keep track of the values of the data in the cells
+      String num1 = csvRepresentation.getValue(col1,row1);
+      String num2 = csvRepresentation.getValue(col2,row2);
+      //finds the sum as an int and then converted into a string
+      int difference = Integer.parseInt(num1) - Integer.parseInt(num2);
+      String s = difference + "";
       Command c = new Command(oldval, s , storeCol, storeRow);
       csvRepresentation.pushCommand(c);
     }
@@ -156,4 +187,5 @@ public class HandleCommand {
         String old = csvRepresentation.getValue(col, row);
         csvRepresentation.pushCommand(new Command(old, "", col, row));
     }
+
   }
